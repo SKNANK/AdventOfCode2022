fa3c82f25c9856228fbfc3129669dcba853e8bfd
Saif Khan <saif.khan95@hotmail.com>
2022-12-05 07:03:57 +0100

    day5

---
+++ PythonSolutions/day5.py
@@ -0,0 +1,14 @@
+with open("Inputs\day5.txt") as l:
+    moves = [move.rstrip('\n') for move in l]
+with open("Inputs\day52.txt") as e:
+    sections = [section.rstrip('\n') for section in e]
+
+for i in moves:
+    full = i.split(',')
+    for _ in range(int(full[0])):
+        sections[int(full[2])-1] = sections[int(full[2])-1] + (sections[int(full[1])-1][-1])
+        sections[int(full[1])-1] = sections[int(full[1])-1][:-1] 
+
+print(sections)
+        
+        
